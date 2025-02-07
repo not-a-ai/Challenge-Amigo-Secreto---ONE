d023c3a4ff35f8027aa45467ab0ee9dd59ee111b
Aléxia <alexia.cassia09@gmail.com>
2025-02-07 15:51:03 -0400

    feat: lógica para adicionar amigos salvando em localStorage

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
+function adicionarAmigo() {
+  let novoAmigo = document.getElementById("amigo").value.trim();
+
+  if (novoAmigo !== "") {
+    let amigosSalvos = JSON.parse(localStorage.getItem("amigos")) || [];
+    console.log(typeof(amigosSalvos))
+
+    amigosSalvos.push(novoAmigo);
+    console.log(amigosSalvos)
+
+    localStorage.setItem("amigos", JSON.stringify(amigosSalvos));
+    atualizarLista();
+
+    document.getElementById("amigo").value = "";
+  } else {
+    alert("Digite um nome antes de adicionar")
+  }
+}
+
+function atualizarLista() {
+
+
+}
